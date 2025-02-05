dba5003bba5854398dc8b99d1776ab7e683ea92e
saibharathireddy <reddysaibharathi8@gmail.com>
2025-02-05 18:21:34 +0530

    Add files via upload

---
+++ projectwordcount.py
@@ -0,0 +1,7 @@
+test_string = "Python Is Wonderful Language:"
+#original string
+print ("The original string is : " + test_string)
+# using split() function
+res = len(test_string.split())
+# total no of words
+print ("The number of words in string are : " + str(res))
